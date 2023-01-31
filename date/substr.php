df60e94583dc4ffb75e4e98868028ad295e7af2d
Sudip Parajuli <sudip.parajulee@gmail.com>
2023-01-31 11:16:25 +0545

    finally

---
+++ date/substr.php
@@ -0,0 +1,11 @@
+<?php
+$text = "Lumbini";
+echo substr($text,0,2); //returns Lu
+echo "<br>";
+echo substr($text,2,2); //returns mb
+echo "<br>";
+echo substr($text,2); //returns mbini
+echo "<br>";
+echo substr($text,-1); //returns i
+
+?>
